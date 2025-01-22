5252281a259f058a0c8a2d6ee66e8d5b6b6962c8
Prajwal2003 <prajwalgh13@gamil.com>
2025-01-22 23:25:50 +0530

    Complete

--- mongocheck.py
+++ mongocheck.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
 import yaml
 
-# Connect to the local MongoDB instance (default URI: mongodb://localhost:27017/)
 client = MongoClient('mongodb+srv://admin:RtnPUAi3LSI7n7rx@twitter-scrapping.cpib0.mongodb.net/?retryWrites=true&w=majority&appName=Twitter-Scrapping')
 
 with open("creds.yaml", "r") as file:
